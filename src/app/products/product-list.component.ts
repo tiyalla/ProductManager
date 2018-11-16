ecadd4d0fea66b57885d2b60f94507e1716b3009
tonye <tonye.iyalla@yahoo.com>
2018-11-16 12:18:05 -0500

    Add data binding for product list

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -7,4 +7,26 @@ import {Component} from '@angular/core';
 
 export class ProductListComponent{
     title = 'Product List';
+    products: any [] = [{
+        "productId": 2,
+        "productName": "Garden cart",
+        "productCode": "FGN-3941",
+        "releaseDate": "March 18, 2015",
+        "description": "15 gallon a month" ,
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "www.yahoo.com"
+
+    },
+    {
+        "productId": 3,
+        "productName": "Lamp chair",
+        "productCode": "FGN-3941",
+        "releaseDate": "March 18, 2015",
+        "description": "15 gallon a month" ,
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "www.yahoo.com"
+
+    }];
 }
