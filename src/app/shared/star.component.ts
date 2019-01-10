7088c2d9c4b71d2d23e088c63c78143f692497b4
tonye <tonye.iyalla@yahoo.com>
2019-01-09 20:08:51 -0500

    Pass rating data to nested component

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges } from "@angular/core";
+import { Component, OnChanges, Input } from "@angular/core";
 
 @Component({
     selector: 'pm-star',
@@ -6,9 +6,10 @@ import { Component, OnChanges } from "@angular/core";
     styleUrls: ['./star.component.css']
 })
 export class StarComponent implements OnChanges{
-    rating: number = 4;
+    @Input() rating: number;
     starWidth: number;
+    
     ngOnChanges(): void {
-        this.starWidth = this.rating * 75 / 5;
+        this.starWidth = (this.rating * 75) / 5;
     }
 }
