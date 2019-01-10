f7b615bf6e5ab9288d2fbb0cee5b48bba821eff7
tonye <tonye.iyalla@yahoo.com>
2019-01-09 19:53:06 -0500

    Create and initialize star component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -14,6 +14,7 @@ export class ProductListComponent implements OnInit{
     showImage: boolean = false;
     _listFilter: string;
     filteredProducts: IProduct[];
+    
     get listFilter(): string{
         return this._listFilter;
     }
