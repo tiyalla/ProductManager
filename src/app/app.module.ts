57d538db63a916cdbd7cb2e8fde074ec78866ca3
tonye <tonye.iyalla@yahoo.com>
2019-01-12 02:23:36 -0500

    Create product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component'
+import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
