136cc9fb6a530b9532bcb5f0159d985dbd431718
tonye <tonye.iyalla@yahoo.com>
2019-01-12 02:40:08 -0500

    Add product detail class variables

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './products';
 
 @Component({
-  selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: 'Product Detail';
+  product: IProduct;
 
   constructor() { }
 
